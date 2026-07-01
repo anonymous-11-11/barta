2f46992fe1e5bc14f61a8bc66d3ac56d4cdfd5f1
anonymous-11-11 <islam73nowrin@gmail.com>
2026-07-01 15:30:49 +0600

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -879,3 +879,26 @@ window.addEventListener('resize',()=>{msgsDiv.scrollTop=msgsDiv.scrollHeight;});
 // window.addEventListener('beforeunload',e=>{e.preventDefault();e.returnValue='';});
 
 setTimeout(updateUIForRole,1000);
+
+//Prevent Inspection and View Source Code for Normal User
+    document.onkeydown = function(e) {
+  if(e.keyCode == 123) { // F12 key
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { // Ctrl+Shift+C
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+    return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { // Ctrl+U
+    return false;
+  }
+};
+
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
